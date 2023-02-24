471c0d9361193cdd1cbba6cf94124c78a1d88487
NicholasConti <conti94@live.it>
2023-02-24 16:47:38 +0100

    commenti

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,11 +1,11 @@
 "use strict";
-
+//variabili con valore casuale tra 1 e 6
 const numeroUtente = Math.floor(Math.random()* 6 + 1);
 const numeroPc = Math.floor(Math.random()* 6 + 1);
 
 console.log(`L' utente ha estratto: ${numeroUtente}`);
 console.log(`Il PC ha estratto: ${numeroPc}`);
-
+//confronto tra valori estratti
 if (numeroUtente > numeroPc){
     console.log(` L' utente ha vinto!`);
 } else if (numeroUtente < numeroPc){
--- mail/js/main.js
+++ mail/js/main.js
@@ -1,11 +1,11 @@
 "use strict";
-
+//input utente
 const mailUtente = prompt('Inserisci la tua mail');
-
+//array
 const listaMail = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "minny@gmail.com"];
 
 let esitoCheck = false;
-
+// ciclo x confrontare i valori nell' array con l' input utente
 for (let i = 0 ; i < listaMail.length ; i++){
     if  (mailUtente === listaMail[i]){
         esitoCheck = true;
